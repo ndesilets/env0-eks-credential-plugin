3edf2246acb53efc6d32cc2103c5f1dfc8f77170
Nic <nic@ndesilets.com>
2024-02-13 11:49:58 -0800

    ignore existing repo, add icon

--- set-eks-credential.sh
+++ set-eks-credential.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-TARGET_EKS_CLUSTER=$(jq -rM '.TARGET_EKS_CLUSTER' env0.env-vars.json)
-if [[ "$TARGET_EKS_CLUSTER" -eq 'null' ]]; then
+if [[ -z "$TARGET_EKS_CLUSTER" ]]; then
     echo "No target EKS cluster set. Skipping credentials."
     exit 0
-fi
+fi 
 
 aws eks --region us-west-2 update-kubeconfig --name "$TARGET_EKS_CLUSTER"
